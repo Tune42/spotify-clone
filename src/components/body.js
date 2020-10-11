4964d8448dd7695dc4f6319820144dfe487ebb8b
Tune42 <59482059+Tune42@users.noreply.github.com>
2020-10-11 01:12:31 -0500

    finished all but the hamburger menu implementation

--- src/components/body.js
+++ src/components/body.js
@@ -5,12 +5,13 @@ const Body = () => {
     return(
     <div className='body'>
       <div className='body-container'>
-        <h1 className='mobile-title'>Another Spotify</h1>
         <h1 className='body-title'>Another</h1>
         <h1 className='body-title'>Spotify</h1>
+        <h1 className='mobile-title'>Another Spotify</h1>
         <p>Millions of songs and podcasts. Partially accessible.</p>
         <button className='body-button'>Log In Here</button>
       </div>
+      <div className='padding'></div>
     </div>
     )
 }
