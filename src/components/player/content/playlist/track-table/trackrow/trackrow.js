6d2171de860e405ee5a80c0f69b20e2a651e2ac1
Tune42 <59482059+Tune42@users.noreply.github.com>
2020-11-09 01:18:16 -0600

    refine hover state

--- src/components/player/content/playlist/track-table/trackrow/trackrow.js
+++ src/components/player/content/playlist/track-table/trackrow/trackrow.js
@@ -10,38 +10,34 @@ const convertMS = (ms) => {
 
 const TrackRow = ({index, name, album, duration, playerState, uri, API, contextURI}) => {
     const [button, setButton] = useState();
-    const [label, setLabel] = useState();
-    const [x, setX] = useState(index + 1);
+    const [label, setLabel] = useState(index + 1);
+    const [hover, setHover] = useState(false);
 
     useEffect(() => {
         if (playerState.trackURI === uri && !playerState.paused) {
             setButton(<i className='fa fa-pause'></i>)
-            // setLabel(<i className='fa fa-play' style={{color: '#1ED760'}}></i>);
-            // setX(<i className='fa fa-play' style={{color: '#1ED760'}}></i>);
             setLabel(<EqualizerIcon fontSize='inherit' htmlColor=' #1ED760' />);
-            setX(<EqualizerIcon fontSize='inherit' htmlColor=' #1ED760' />);
         } else {
             setButton(<i className='fa fa-play' style={{color: '#1ED760'}}></i>);
             setLabel(index + 1);
-            setX(index + 1);
         }
-    }, [index , playerState, uri]);
+    }, [index, playerState, uri]);
 
     const toggle = () => {
         if (playerState.paused || playerState.trackURI !== uri) {
             API.play({context_uri: contextURI, offset: {position: index}});
-            setX(<i className='fa fa-pause'></i>)
+            setButton(<i className='fa fa-pause'></i>)
         } else {
             API.pause();
-            setX(<i className='fa fa-play'></i>);
+            setButton(<i className='fa fa-play'></i>);
         }
     }
 
     return(
         <TableRow key={index + " - " + name}
-        onMouseEnter={() => setX(button)}
-        onMouseLeave={() => setX(label)}>
-            <TableCell onClick={() => toggle()}>{x}</TableCell>
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}>
+            <TableCell onClick={() => toggle()}>{hover ? button : label}</TableCell>
             <TableCell>{name}</TableCell>
             <TableCell>{album}</TableCell>
             <TableCell align="center">{convertMS(duration)}</TableCell>
