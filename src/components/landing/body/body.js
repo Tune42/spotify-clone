b601576f434368bf2989dbe8faf2bc970cb776e5
Tune42 <59482059+Tune42@users.noreply.github.com>
2020-11-10 12:42:47 -0600

    update body text

--- src/components/landing/body/body.js
+++ src/components/landing/body/body.js
@@ -9,7 +9,7 @@ const Body = ({authLink}) => {
         <h1 className='body-title'>Another</h1>
         <h1 className='body-title'>Spotify</h1>
         <h1 className='mobile-title'>Another Spotify</h1>
-        <p>Millions of songs and podcasts. Partially accessible.</p>
+        <p>Millions of songs and podcasts from Spotify's API.</p>
         <a href={authLink}><LandingButton /></a>
       </div>
       <div className='padding'></div>
