e0f2f334701d248cc7108ceed656c0c3de61d04a
Tune42 <59482059+Tune42@users.noreply.github.com>
2020-11-14 13:40:26 -0600

    add home content

--- src/components/player/content/card.js
+++ src/components/player/content/card.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 const Card = ({album, changeContextURI}) => {
+    let description;
+    if (album.release_date) {
+        description = <h5 className='card-description'>Released {album.release_date}</h5>;
+    } else {
+        description = <h5 className='card-description'>{album.description}</h5>
+    }
+
+    let image;
+    if (album.images.length > 1) {
+        image = <img  className='card-image' src={album.images[1].url} alt="playlist" />
+    } else {
+        image = <img className='card-image' src={album.images[0].url} alt="playlist" />
+    }
+
     return(
         <div className='card' onClick={() => changeContextURI(album.uri)}>
-            <img  className='card-image' src={album.images[1].url} alt="playlist" />
+            {image}
             <div className='card-text'>
                 <h3 className='card-title'>{album.name}</h3>
-                <h5 className='card-description'>Released {album.release_date}</h5>
+                {description}
             </div>
         </div>
     )
--- src/components/player/content/home.js
+++ src/components/player/content/home.js
@@ -1,19 +1,56 @@
 import React, {useState, useEffect} from 'react';
+import Card from '../../../components/player/content/card';
 
 const Home = ({API, changeContextURI}) => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        // getMySavedAlbums
-        API.getUserPlaylists()
-        .then(res => {
-            console.log(res);
-        }).catch(err => console.log(err));
+        if (data === null) {
+            const newData = {};
+
+            async function fetchData() {
+                await API.getUserPlaylists()
+                .then(res => {
+                    console.log(res);
+                    newData['playlists'] = res.items;
+                }).catch(err => console.log(err));
+
+                await API.getMySavedAlbums()
+                .then(res => {
+                    newData['savedAlbums'] = res.items;
+                }).catch(err => console.log(err));
+                setData(newData);
+            }
+
+            fetchData();
+        }
     });
+
+    let savedAlbums;
+    let playlists;
+
+    if (data !== null && data.savedAlbums) {
+        savedAlbums = data.savedAlbums.map((album, index) => {
+            return <Card key={index + ' ' + album.album.name} album={album.album} changeContextURI={changeContextURI} />;
+        })
+    }
+
+    if (data !== null && data.playlists) {
+        playlists = data.playlists.map((playlist, index) => {
+            return <Card key={index + ' ' + playlist.name} album={playlist} changeContextURI={changeContextURI} />;
+        })
+    }
 
     return(
         <div className="content-container">
-            <h1>Home</h1>
+            <div className="card-category">
+                <div className="card-category-title"><h2>Saved Albums</h2></div>
+                <div className='card-category-items'>{savedAlbums}</div>
+            </div>
+            <div className="card-category">
+                <div className="card-category-title"><h2>Playlists</h2></div>
+                <div className='card-category-items'>{playlists}</div>
+            </div>
         </div>
     )
 }
