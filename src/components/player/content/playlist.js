f604bd850be8204f1f64f425d94c00ec98b459a2
Tune42 <59482059+Tune42@users.noreply.github.com>
2020-11-15 20:27:56 -0600

    add follow and unfollow artists and playlists feature

--- src/components/player/content/playlist.js
+++ src/components/player/content/playlist.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import TrackTable from './tracktable';
 
 const Playlist = ({image, type, name, description, tracks, playerState, API, contextURI, artistURI, changeContextURI}) => {
+    const [following, setFollowing] = useState(null);
+
+    useEffect(() => {
+        async function getFollowStatus() {
+            if (type === 'album') {
+                await API.containsMySavedAlbums([contextURI.split(':').pop()])
+                .then(res => setFollowing(res[0]))
+                .catch(err => console.log(err));
+            } else {
+                let userID;
+                await API.getMe()
+                .then(res => userID = res.id)
+                .catch(err => console.log(err));
+                await API.areFollowingPlaylist(contextURI.split(':').pop(), [userID])
+                .then(res => setFollowing(res[0]))
+                .catch(err => console.log(err));
+            }
+        }
+        getFollowStatus();
+    });
+
+    const toggleFollowStatus = () => {
+        if (!following) {
+            if (type === 'album') {
+                API.addToMySavedAlbums([contextURI.split(':').pop()])
+                .then(() => {
+                    setFollowing(!following);
+                }).catch(err => console.log(err));
+            } else {
+                API.followPlaylist(contextURI.split(':').pop())
+                .then(() => {
+                    setFollowing(!following);
+                }).catch(err => console.log(err));
+            }
+        } else {
+            if (type === 'album') {
+                API.removeFromMySavedAlbums([contextURI.split(':').pop()])
+                .then(() => {
+                    setFollowing(!following);
+                }).catch(err => console.log(err));
+            } else {
+                API.unfollowPlaylist(contextURI.split(':').pop())
+                .then(() => {
+                    setFollowing(!following);
+                }).catch(err => console.log(err));
+            }
+        }
+    }
+
     let subtitle;
     artistURI ? 
     subtitle = <p className='artist-link' onClick={() => changeContextURI(artistURI)}>{description}</p> : 
@@ -14,7 +63,10 @@ const Playlist = ({image, type, name, description, tracks, playerState, API, con
                     <img src={image} alt="Cover Art" width='200px' />
                 </div>
                 <div className='playlist-info'>
-                    <p style={{fontWeight: 400, textTransform: "uppercase"}}><small>{type}</small></p>
+                    <p style={{fontWeight: 400, textTransform: "uppercase"}}>
+                        <small>{type}</small>
+                        <i className="fa fa-heart" style={following ? {color: '#1ED760'} : {}} onClick={toggleFollowStatus}></i>
+                    </p>
                     <h1 className='playlist-title'>{name}</h1>
                     {subtitle}
                 </div>
